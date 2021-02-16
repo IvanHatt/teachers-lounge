830458f8e29131fdeccad48d6378b359d1c8b799
Ivan Hattemer <ivanhattemer@gmail.com>
2021-02-16 17:17:26 +0200

    small chges

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -17,11 +17,13 @@ import "components/css/navbar.css";
 const Navbar = (props) => {
   const { user } = props;
   const [scrollState, setScrollState] = useState("nav-up");
+  const [toggle, setToggle] = useState(false);
 
   useEffect(() => {
     let listener = document.addEventListener("scroll", (e) => {
-      var scrolled = document.scrollingElement.scrollTop;
-      if (scrolled >= 120) {
+      let scrolled = document.scrollingElement.scrollTop;
+      let portsize = window.innerWidth;
+      if (scrolled >= 120 && portsize > 1000) {
         if (scrollState !== "nav-down") {
           setScrollState("nav-down");
         }
@@ -38,11 +40,11 @@ const Navbar = (props) => {
   return (
     <div className="main-navbar">
       <nav
-        className="navbar fixed-top navbar-expand-lg"
+        className="navbar fixed-top navbar-expand-lg justify-content-between"
         style={
           scrollState === "nav-up"
             ? {
-                padding: "40px",
+                padding: "20px 40px 0 40px",
                 transition: "0.3s ease-in-out",
               }
             : {
@@ -50,135 +52,150 @@ const Navbar = (props) => {
               }
         }
       >
-        <Link className="navbar-brand" to="/">
-          <img src={logo} alt="logo" />
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span>
-            <FontAwesomeIcon icon={faAngleDoubleDown} />
-          </span>
-        </button>
-
-        <div
-          className="collapse navbar-collapse justify-content-between"
-          id="navbarNav"
-        >
-          <ul className="navbar-nav" style={scrollState === "nav-up" ? {display: 'inherit'}: {display: 'none'}}>
-            <li className="nav-item mr-3">
-              <NavLink className="nav-link" to="/" exact>
-                Home
-              </NavLink>
-            </li>
-            <li className="nav-item mr-3">
-              <NavLink className="nav-link" to="/about">
-                About
-              </NavLink>
-            </li>
-            <li className="nav-item mr-3">
-              <NavLink className="nav-link" to="/explore">
-                Explore
-              </NavLink>
-            </li>
-          </ul>
+        <div className="navbar-brand">
+          <button
+            className="user-menu d-lg-none"
+            type="button"
+            onClick={() => setToggle(!toggle)}
+          >
+            <span>
+              <FontAwesomeIcon icon={faAngleDoubleDown} />
+            </span>
+          </button>
+          <Link to="/">
+            <img src={logo} alt="logo" />
+          </Link>
         </div>
-          <div className="mt-3 float-right">
-            {!user && (
-              <ul className='navbar-nav'>
-                <li>
-                  <NavLink className="btn-styled opaque"  to="/signin">
-                    Sign In
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink className="btn-styled" to="/signup">
-                    Sign Up
-                  </NavLink>
-                </li>
-              </ul>
-            )}
-            {user && (
-              <React.Fragment>
-                <div className="dropdown dropleft">
-                  <button
-                    className="user-menu dropdown-toggle"
-                    type="button"
-                    id="dropdownMenuButton"
-                    data-toggle="dropdown"
-                    aria-haspopup="true"
-                    aria-expanded="false"
-                  >
-                    {user.prof ? (
-                      <img
-                        src={`${apiUrl}/profs/${user.profImage}`}
-                        alt="user"
+
+        <ul
+          className={`navbar-nav d-none ${
+            scrollState === "nav-up" ? "d-lg-flex" : ""
+          }`}
+        >
+          <li className="nav-item mr-3">
+            <NavLink className="nav-link" to="/" exact>
+              Home
+            </NavLink>
+          </li>
+          <li className="nav-item mr-3">
+            <NavLink className="nav-link" to="/about">
+              About
+            </NavLink>
+          </li>
+          <li className="nav-item mr-3">
+            <NavLink className="nav-link" to="/explore">
+              Explore
+            </NavLink>
+          </li>
+        </ul>
+
+        <div className="mt-3 float-right">
+          {!user && (
+            <ul className="navbar-nav">
+              <li>
+                <NavLink className="btn-styled opaque" to="/signin">
+                  Sign In
+                </NavLink>
+              </li>
+              <li>
+                <NavLink className="btn-styled" to="/signup">
+                  Sign Up
+                </NavLink>
+              </li>
+            </ul>
+          )}
+          {user && (
+            <React.Fragment>
+              <div className="dropdown dropleft">
+                <button
+                  className="user-menu dropdown-toggle"
+                  type="button"
+                  id="user-menu"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  {user.prof ? (
+                    <img
+                      src={`${apiUrl}/profs/${user.profImage}`}
+                      alt="user"
+                      className="mb-2"
+                    />
+                  ) : (
+                    <span className="mb-2">
+                      Student
+                      <FontAwesomeIcon
+                        icon={faUserCircle}
+                        size="3x"
+                        style={{ color: "#f9f871" }}
                       />
-                    ) : (
-                      <span>
-                        Student
-                        <FontAwesomeIcon
-                          icon={faUserCircle}
-                          size="3x"
-                          style={{ color: "#f9f871" }}
-                        />
-                      </span  >
-                    )}
-                  </button>
-                  <div
-                    className="dropdown-menu"
-                    aria-labelledby="dropdownMenuButton"
-                  >
-                    <span className="dropdown-header">{user.profName}</span>
-                    <span className="dropdown-header">{user.profEmail}</span>
-                    <hr />
-                    <NavLink className="dropdown-item" to="/favorites">
-                      <div className="icon">
-                        <FontAwesomeIcon icon={faStar} />
-                      </div>
-                      Favorites
-                    </NavLink>
-                    <NavLink className="dropdown-item" to="/explore">
-                      <div className="icon">
-                        <FontAwesomeIcon icon={faSearch} />
-                      </div>
-                      Explore
-                    </NavLink>
-                    {user && user.prof && (
-                      <React.Fragment>
-                        <NavLink className="dropdown-item" to="/my-profs">
-                          <div className="icon">
-                            <FontAwesomeIcon icon={faAddressCard} />
-                          </div>
-                          My Cards
-                        </NavLink>
-                        <NavLink className="dropdown-item" to="/prof-create">
-                          <div className="icon">
-                            <FontAwesomeIcon icon={faGraduationCap} />
-                          </div>
-                          New Card
-                        </NavLink>
-                      </React.Fragment>
-                    )}
-                    <hr />
-                    <NavLink className="dropdown-item" to="/logout">
-                      <div className="icon">
-                        <FontAwesomeIcon icon={faSignOutAlt} />
-                      </div>
-                      Logout
-                    </NavLink>
-                  </div>
+                    </span>
+                  )}
+                </button>
+                <div className="dropdown-menu" aria-labelledby="user-menu">
+                  <span className="dropdown-header">{user.profName}</span>
+                  <span className="dropdown-header">{user.profEmail}</span>
+                  <hr />
+                  <NavLink className="dropdown-item" to="/favorites">
+                    <div className="icon">
+                      <FontAwesomeIcon icon={faStar} />
+                    </div>
+                    Favorites
+                  </NavLink>
+                  <NavLink className="dropdown-item" to="/explore">
+                    <div className="icon">
+                      <FontAwesomeIcon icon={faSearch} />
+                    </div>
+                    Explore
+                  </NavLink>
+                  {user && user.prof && (
+                    <React.Fragment>
+                      <NavLink className="dropdown-item" to="/my-profs">
+                        <div className="icon">
+                          <FontAwesomeIcon icon={faAddressCard} />
+                        </div>
+                        My Cards
+                      </NavLink>
+                      <NavLink className="dropdown-item" to="/prof-create">
+                        <div className="icon">
+                          <FontAwesomeIcon icon={faGraduationCap} />
+                        </div>
+                        New Card
+                      </NavLink>
+                    </React.Fragment>
+                  )}
+                  <hr />
+                  <NavLink className="dropdown-item" to="/logout">
+                    <div className="icon">
+                      <FontAwesomeIcon icon={faSignOutAlt} />
+                    </div>
+                    Logout
+                  </NavLink>
                 </div>
-              </React.Fragment>
-            )}
-          </div>
+              </div>
+            </React.Fragment>
+          )}
+        </div>
       </nav>
+      {toggle && (
+        <div className="mob-menu d-lg-none">
+          <div className="mob-item it-one">
+            <NavLink className="nav-link" to="/" exact>
+              Home
+            </NavLink>
+          </div>
+          <div className="mob-item it-two">
+            <NavLink className="nav-link" to="/about">
+              About
+            </NavLink>
+          </div>
+          <div className="mob-item it-three">
+            <NavLink className="nav-link" to="/explore">
+              Explore
+            </NavLink>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
